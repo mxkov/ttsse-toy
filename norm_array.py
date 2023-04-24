ecd0383ffa8583b61a80c3c9a1368ea5ec291622
mxkov <hawo0451@gmail.com>
2023-04-24 15:00:13 +0200

    Set random state correctly. Fixes issue #1

--- norm_array.py
+++ norm_array.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def norm_array(n=10, mean=0.0, std=1.0, randomstate=42):
-	np.random_seed(randomstate)
+	np.random.seed(randomstate)
 	return np.random.normal(loc=mean, scale=std, size=n)
 
 def plot_hist(x, filename='hist.png'):
