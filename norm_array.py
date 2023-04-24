6b351485383ebfe368b2480eb786299b923ce152
mxkov <hawo0451@gmail.com>
2023-04-24 14:58:41 +0200

    Save plot to file

--- norm_array.py
+++ norm_array.py
@@ -5,5 +5,7 @@ def norm_array(n=10, mean=0.0, std=1.0, randomstate=42):
 	np.random_seed(randomstate)
 	return np.random.normal(loc=mean, scale=std, size=n)
 
-def plot_hist(x):
+def plot_hist(x, filename='hist.png'):
 	plt.hist(x, bins='auto')
+	plt.savefig(filename, dpi=200, bbox_inches='tight')
+	plt.close()
