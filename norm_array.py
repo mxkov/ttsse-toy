9c223250c8d48ef9508d030c04e4da4c97aa3df3
mxkov <hawo0451@gmail.com>
2023-04-24 14:35:02 +0200

    Add plotting function stub

--- norm_array.py
+++ norm_array.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def norm_array(n=10, mean=0.0, std=1.0, randomstate=42):
 	return np.random.normal(loc=mean, scale=std, size=n)
+
+def plot_hist(x):
+	raise NotImplementedError
