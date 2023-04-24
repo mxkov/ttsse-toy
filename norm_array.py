2943c1fb0bb0273c10e1188716faa1c3022454ed
mxkov <hawo0451@gmail.com>
2023-04-24 14:56:29 +0200

    Make plotting function actually plot stuff

--- norm_array.py
+++ norm_array.py
@@ -6,4 +6,4 @@ def norm_array(n=10, mean=0.0, std=1.0, randomstate=42):
 	return np.random.normal(loc=mean, scale=std, size=n)
 
 def plot_hist(x):
-	raise NotImplementedError
+	plt.hist(x, bins='auto')
